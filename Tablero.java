34d6213c951e75549566c515ce2124692b43f48d
FoxyYTs <foxy200442@gmail.com>
2023-09-24 09:06:44 -0500

    gj

--- Tablero.java
+++ Tablero.java
@@ -1,21 +1,28 @@
 import javax.swing.JPanel;
 import javax.swing.BorderFactory;
-import java.swing.JFrame;
+import javax.swing.*;
+
 import java.awt.*;
 public class Tablero{
     public static void main(String[] args){
-        JPanel[][] Cuadrtos=new JPanel[20][10];
+        JPanel[][] Cuadritos=new JPanel[20][10];
         for(int i=0;i<20;i++){
             for(int j=0;j<10;j++){
                 Cuadritos[i][j]= new JPanel();
                 Cuadritos[i][j].setBorder(BorderFactory.createLineBorder(Color.RED));
                 Cuadritos[i][j].setBackground(Color.GRAY);
-                Cuadritos[i][j].setPreferedSize(new Dimension(30,30));
+                Cuadritos[i][j].setPreferredSize(new Dimension(30,30));
             }
         }
         JFrame Mostrar=new JFrame("Tetris");
         Mostrar.setLayout(new GridLayout(20,10));
+        for (JPanel[] fila: Cuadritos) {
+            for(JPanel panel: fila){
+                Mostrar.add(panel);
+            }
+        }
         Mostrar.pack();
+        Mostrar.setResizable(false);
         Mostrar.setVisible(true);
     }
 }
