ab3f71768a49f39af9c6937e9837517689f31db7
AnaMgS02 <anamariagomez102@gmail.com>
2023-10-01 14:42:40 -0500

    #

--- TallerProgramacion/baseDatos.py
+++ TallerProgramacion/baseDatos.py
@@ -1,13 +1,14 @@
 import sqlite3
 
-conn=sqlite3.connect('musicadb')
+conn=sqlite3.connect('TallerProgramacion/musicadb.db')
+
 c=conn.cursor()
 
 c.execute("""CREATE TABLE IF NOT EXISTS musica (
-        titulo VARCHAR
-        artista
-        duracion
-        fecha_lanzamiento
-        formato
-        rating
+        titulo VARCHAR(45),
+        artista VARCHAR(45),
+        duracion VARCHAR (45),
+        año_lanzamiento INT,
+        formato VARCHAR(5),
+        genero VARCHAR(45)
 )""")
