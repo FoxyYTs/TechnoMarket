36e60adbee26798f8157c3cf3dd221d094e9abd7
FoxyYTs <foxy200442@gmail.com>
2023-10-01 13:06:14 -0500

    sdgf

--- TallerProgramacion/CRUD.py
+++ TallerProgramacion/CRUD.py
@@ -1,10 +1,2 @@
-import pandas as pd
-
-Hoja1=pd.read_html("https://es.wikipedia.org/wiki/Estatura")[1]
-Hoja2=pd.read_html("https://www.cuandoenelmundo.com/herramientas/codigos-para-llamadas-internacionales")[0]
-Hoja3=pd.read_html("https://minecraft-ids.grahamedgecombe.com")[0]
-
-with pd.ExcelWriter("archivoXLSX.xlsx") as writer:
-    Hoja1.to_excel(writer,sheet_name="Altura")
-    Hoja2.to_excel(writer,sheet_name="Codigo llamada")
-    Hoja3.to_excel(writer,sheet_name="Minecraft")
+import sqlite3
+conn = sqlite3.connect(".\Taller.db")
