e76a59e6fdfecbb8a374826ffb21d8f1c8708dbc
AnaMgS02 <anamariagomez102@gmail.com>
2023-10-01 17:52:57 -0500

    db

--- TallerProgramacion/CRUD.py
+++ TallerProgramacion/CRUD.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from pandas import ExcelWriter
+import baseDatos
 
 class Crud:
     def crear():
@@ -7,3 +8,15 @@ class Crud:
         ExcelWriter("TallerProgramacion/" + titulo + ".xlsx")
 
     crear()
+    def registrar():
+        c.execute("""INSERT INTO musica(id_musica, titulo, artista_banda, duracion, ano_lanzamiento, formato, genero)
+                  values(cont_musica, titulo, artista_banda, duracion, ano_lanzamiento, formato, genero)""")
+
+
+    def mostrar():
+        for i 
+    
+
+    def actualizar():
+
+    def eliminar():
--- TallerProgramacion/baseDatos.py
+++ TallerProgramacion/baseDatos.py
@@ -5,10 +5,23 @@ conn=sqlite3.connect('TallerProgramacion/musicadb.db')
 c=conn.cursor()
 
 c.execute("""CREATE TABLE IF NOT EXISTS musica (
-        titulo VARCHAR(45),
-        artista VARCHAR(45),
-        duracion VARCHAR (45),
-        año_lanzamiento INT,
-        formato VARCHAR(5),
-        genero VARCHAR(45)
+        id_musica INT PRIMARY KEY,
+        titulo VARCHAR(45)NOT NULL,
+        artista_banda VARCHAR(45) NOT NULL,
+        duracion VARCHAR (45) NOT NULL,
+        ano_lanzamiento INT NOT NULL,
+        formato VARCHAR(5) NOT NULL,
+        genero VARCHAR(45) NOT NULL
 )""")
+c.execute("""CREATE TABLE IF NOT EXISTS rock(
+          id_rock INT PRIMARY KEY,
+          subgenero VARCHAR(45) NOT NULL,
+          conciertos_dados INT NOT NULL,
+          pais_origen VARCHAR(45) NOT NULL,
+          letra TEXT NOT NULL,
+          musica INT NOT NULL,
+          FOREIGN KEY (musica) REFERENCES musica (id_musica)
+)""")
+
+
+conn.close()
