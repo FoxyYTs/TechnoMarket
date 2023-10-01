ebb61cce3f17c02862a1190967305911cc527504
FoxyYTs <foxy200442@gmail.com>
2023-10-01 14:39:24 -0500

    asd

--- TallerProgramacion/CRUD.py
+++ TallerProgramacion/CRUD.py
@@ -1,2 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(".\Taller.db")
+import pandas as pd
+from pandas import ExcelWriter
+
+def crear():
+    titulo = input('titulo: ')
+    ExcelWriter("TallerProgramacion/" + titulo + ".xlsx")
+
+crear()
