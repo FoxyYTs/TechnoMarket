a544fa4835f0d2e5a60d49a2f04daf7aae933058
FoxyYTs <foxy200442@gmail.com>
2023-10-01 14:39:57 -0500

    asd

--- TallerProgramacion/CRUD.py
+++ TallerProgramacion/CRUD.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from pandas import ExcelWriter
 
-def crear():
-    titulo = input('titulo: ')
-    ExcelWriter("TallerProgramacion/" + titulo + ".xlsx")
+class Crud:
+    def crear():
+        titulo = input('titulo: ')
+        ExcelWriter("TallerProgramacion/" + titulo + ".xlsx")
 
-crear()
+    crear()
