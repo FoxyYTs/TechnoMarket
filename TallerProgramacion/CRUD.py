e323504b6dd75c209cf8f598b270e41c3319b5dc
FoxyYTs <foxy200442@gmail.com>
2023-10-01 23:07:36 -0500

    adf

--- TallerProgramacion/CRUD.py
+++ TallerProgramacion/CRUD.py
@@ -4,23 +4,6 @@ import os
 import sqlite3
 
 class Crud:
-    def crear():
-        df1 = pd.DataFrame({'A':['A0', 'A1', 'A2','A3'],
-        'B':['B0', 'B1', 'B2','B3'],
-        'C':['C0', 'C1', 'C2','C3'],
-        'D':['D0', 'D1', 'D2','D3']})
-
-        df2 = pd.DataFrame({'A':['A0', 'A1', 'A2','A3'],
-        'B':['B0', 'B1', 'B2','B3'],
-        'C':['C0', 'C1', 'C2','C3'],
-        'D':['D0', 'D1', 'D2','D3']})
-        titulo = input('titulo: ')
-        with ExcelWriter("TallerProgramacion/" + titulo + ".xlsx") as writer:
-            df1.to_excel(writer,sheet_name="Clase Padre")
-            df2.to_excel(writer,sheet_name="Clase Hijo")
-    def leer():
-        print(pd.read_excel("TallerProgramacion/Karl.xlsx"))
-    
     def create_table_musica():
         conn=sqlite3.connect('TallerProgramacion/musicadb.db')
         c=conn.cursor()
