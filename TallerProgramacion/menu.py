37291e70668252bd20498eaa68b0312d09e51be9
FoxyYTs <foxy200442@gmail.com>
2023-10-01 19:44:30 -0500

    da

--- TallerProgramacion/menu.py
+++ TallerProgramacion/menu.py
@@ -1,13 +1,18 @@
 import os
 import time
+form Musica import 
 opcion = 1
 while (opcion != 5):
     selecto = True
     key = input("Ingresa la llave foranea: ")
+    try:
+        cancion = Musica(id_musica)
+    except:
+        print("ERROR FATAL")
     os.system("cls")
     opcion = int(input("Que opcion deseas realizar\n1) Crear Dato en la BD\n2) Leer Dato en la BD\n3) Actualizar Dato en la BD\n4) Eliminar Dato en la BD\n5) Salir"))
     if opcion == 1:
-        print ("hola")
+        
     elif opcion == 5:
         os.system("cls")
         time.sleep(1)
