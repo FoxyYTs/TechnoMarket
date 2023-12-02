a8e9cb0c57cdb7019c2bc868aa5e1a1ba04e82d0
FoxyYTs <foxy200442@gmail.com>
2023-12-02 10:02:01 -0500

    asdada

--- Nueva carpeta (6)/AppPortal/urls.py
+++ Nueva carpeta (6)/AppPortal/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from Web.views import pagInicio, pagLogin, pagRegistro
-from Producto.views import pagProductos, infoCategorias, consultaP
+from Producto.views import pagProductos, infoCategorias, consultaP, godmin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('godmin/', godmin),
     path('', pagInicio),
     path('login/', pagLogin),
     path('registro/', pagRegistro),
