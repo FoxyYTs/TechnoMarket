761a56e63a13d3481c8adda74afc7d7e3725476b
FoxyYTs <foxy200442@gmail.com>
2024-03-01 10:00:58 -0500

    asd

--- Examen1/ListaG.java
+++ Examen1/ListaG.java
@@ -1,7 +1,11 @@
 package Examen1;
 
+import java.util.*;
+
 public class ListaG {
+    private Scanner leer = new Scanner(System.in);
     public Nodo head;
+    private char last;
 
     public ListaG() {
         head = null;
@@ -12,35 +16,38 @@ public class ListaG {
         Nodo x = new Nodo(' ');
         head = x;
         Nodo ultimo = x;
-        int n = str.length();
-        for (int i = 1; i < n; i++) {
-            switch (str.charAt(i)) {
-                case ',':
-                    x = new Nodo(' ');
-                    ultimo.setNext(x);
-                    ultimo = x;
-                    if (stack.peek() == null){
-                        ultimo.setHead(head);
-                    } else {
-                        ultimo.setHead(stack.peek());
-                    }
-                    break;
-                case '(':
-                    stack.push(ultimo);
-                    x = new Nodo(' ');
-                    ultimo.setTag(1);
-                    ultimo.setsubList(x);
-                    ultimo = x;
-                    break;
-                case ')':
-                    ultimo = stack.pop();
-                    break;
-                default:
-                    System.out.println(str.charAt(i));
-                    ultimo.setTag(0);
-                    ultimo.setDato(str.charAt(i));
+        System.out.println(str);
+        
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == ','){
+                x = new Nodo(' ');
+                ultimo.setNext(x);
+                ultimo = x;
+                if (stack.peek() == null){
+                    ultimo.setHead(head);
+                } else {
                     ultimo.setHead(stack.peek());
-                    break;
+                }
+            } else if (str.charAt(i) == '('){
+                stack.push(ultimo);
+                x = new Nodo(' ');
+                ultimo.setTag(1);
+                ultimo.setsubList(x);
+                ultimo = x;
+            } else if(str.charAt(i) == ')'){
+                ultimo = stack.pop();
+            } else if (Character.isUpperCase(str.charAt(i))){
+                System.out.println("Cual es la lista equivalente a: " + str.charAt(i));
+                String a = leer.nextLine();
+                str = reemplazaCaracter(str, String.valueOf(str.charAt(i)), a);
+                i -= 1;
+                System.out.println(str);
+
+            }else {
+                last = str.charAt(i);
+                ultimo.setTag(0);
+                ultimo.setDato(str.charAt(i));
+                ultimo.setHead(stack.peek());
             }
         }
     }
@@ -50,12 +57,19 @@ public class ListaG {
             return;
         }
         Nodo pointer = head;
+        Nodo temp = null;
         while(pointer != null){
+            if (pointer.getDato() == last){
+                System.out.println(last);
+                break;
+            }
             if (pointer.getDato() != ' '){
                 System.out.print(pointer.getDato());
             }
-            if (pointer.getsubList() != null){
+            
+            if (pointer.getsubList() != null && pointer != temp){
                 pointer = pointer.getsubList();
+                temp = pointer;
                 System.out.print(pointer.getDato());
             } else if (pointer.getNext() == null){
                 pointer = pointer.getHead();
@@ -65,5 +79,20 @@ public class ListaG {
                 
             }
         }
+    }
+
+    public static String reemplazaCaracter(String str, String reemplazado, String reemplazador ) {
+        char charToreplace = reemplazado.charAt(0);
+        String output = "";
+    
+        for (int i = 0; i < str.length(); i++) {         
+             if( str.charAt(i) == charToreplace) {
+                 output += reemplazador; 
+                 str.substring(i+1, str.length());
+             }else{
+                 output += str.charAt(i);
+            }            
+        }
+        return output;
     }
 }
--- Examen1/Main.java
+++ Examen1/Main.java
@@ -3,26 +3,13 @@ package Examen1;
 public class Main {
     public static void main(String[] args) {
         ListaG list = new ListaG();
-        ListaG list2 = new ListaG();
-        list2.head = null;
 
-        list.insertarListaG(("(a,(b,c),(x),d)"));
+        list.insertarListaG(("(a,A,(b,c,(d,e,f),C))"));
         list.recListG();
-
-        if (list2.head == null){
-            System.out.println();
-            System.out.println("esta lista esta vacia");
-        }
-
-        list2.head = list.head;
-
-        if (list2.head == null){
-            System.out.println("esta lista esta vacia");
-        }
-
-        list2.recListG();
     }
-    //(a;A,(b,c,(d,e,f),C))
+
+
+    //(a,A,(b,c,(d,e,f),C))
     //(a,A,(b,c,D,(d,e),f))
     //A =(x,y,z)
     //C =(n,m,L)
