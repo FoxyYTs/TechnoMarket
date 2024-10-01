46e62aed299a63cd080ce3f539f71dc93918c568
AnaMgS02 <anamariagomez102@gmail.com>
2024-10-01 18:33:39 -0500

    s

--- TallerLenguajes2/SGI2/actualizar_insumo.php
+++ TallerLenguajes2/SGI2/actualizar_insumo.php
@@ -1,46 +1,47 @@
 <?php
 include_once("db.php");
 
-// Verificar si se ha recibido el ID del insumo en la URL
-if (isset($_GET['id_insumo'])) {
-    $id_insumo = $_GET['id_insumo'];
+// Verificar si se ha recibido el ID del implemento en la URL
+if (isset($_GET['id_implemento'])) {
+    $id_implemento = $_GET['id_implemento'];
 
     // Conectar a la base de datos
     $conectar = conn();
 
-    // Obtener los datos actuales del insumo
-    $sql = "SELECT * FROM insumo WHERE id_insumo = ?";
+    // Obtener los datos actuales del implemento
+    $sql = "SELECT * FROM implemento WHERE id_implemento = ?";
     $stmt = $conectar->prepare($sql);
-    $stmt->bind_param("i", $id_insumo);
+    $stmt->bind_param("i", $id_implemento);
     $stmt->execute();
     $result = $stmt->get_result();
-    $insumo = $result->fetch_assoc();
+    $implemento = $result->fetch_assoc();
 
     // Verificar si se ha recibido el formulario
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Obtener los datos del formulario
         $foto = $_POST["foto"];
-        $stock = $_POST["stock"];
+        $stock = $_POST["stock_implemento"];
         $ubicacion = $_POST["ubicacion"];
+        $guia_uso= $_POST["guia_uso"];
         $medida = $_POST["und_medida"];
 
-        // Preparar la sentencia SQL para actualizar el insumo
-        $sql_update = "UPDATE insumo SET foto = ?, stock_insumo = ?, ubicacion_fk = ?, und_medida_fk = ? WHERE id_insumo = ?";
+        // Preparar la sentencia SQL para actualizar el implemento
+        $sql_update = "UPDATE implemento SET foto = ?, stock_implemento = ?, ubicacion_fk = ?, guia_uso_lab = ?, und_medida_fk = ? WHERE id_implemento = ?";
         $stmt_update = $conectar->prepare($sql_update);
-        $stmt_update->bind_param("sisii", $foto, $stock, $ubicacion, $medida, $id_insumo);
+        $stmt_update->bind_param("sissii", $foto, $stock, $ubicacion, $guia_uso, $medida, $id_implemento);
 
         // Ejecutar la sentencia SQL
         if ($stmt_update->execute()) {
-            echo '<script>alert("Insumo actualizado correctamente"); window.location.href = "insumos.php";</script>';
+            echo '<script>alert("implemento actualizado correctamente"); window.location.href = "implementos.php";</script>';
         } else {
-            echo '<script>alert("Error al actualizar insumo: ' . $stmt_update->error . '");</script>';
+            echo '<script>alert("Error al actualizar implemento: ' . $stmt_update->error . '");</script>';
         }
         $stmt_update->close();
     }
     $stmt->close();
     $conectar->close();
 } else {
-    echo '<script>alert("No se ha recibido un ID de insumo válido"); window.location.href = "insumos.php";</script>';
+    echo '<script>alert("No se ha recibido un ID de implemento válido"); window.location.href = "implementos.php";</script>';
     exit();
 }
 ?>
@@ -50,7 +51,7 @@ if (isset($_GET['id_insumo'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Actualizar Insumo - SGI LAB MANAGER</title>
+    <title>Actualizar implemento - SGI LAB MANAGER</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="css/style.css">
@@ -66,28 +67,28 @@ if (isset($_GET['id_insumo'])) {
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="nav nav-tabs">
                 <li class="nav-item">
-                    <a class="nav-link" href="insumos.php">INSUMOS</a>
+                    <a class="nav-link" href="implementos.php">implementoS</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="buscar_insumo.html">BUSCAR INSUMO</a>
+                    <a class="nav-link" href="buscar_implemento.html">BUSCAR implemento</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="registro_insumo.php">REGISTRO INSUMO</a>
+                    <a class="nav-link" href="registro_implemento.php">REGISTRO implemento</a>
                 </li>
             </ul>
         </div>
     </nav>
 
     <div class="container mt-5">
-        <h1 class="text-center mb-4">Actualizar Insumo</h1>
-        <form action="actualizar_insumo.php?id_insumo=<?php echo $id_insumo; ?>" method="POST">
+        <h1 class="text-center mb-4">Actualizar implemento</h1>
+        <form action="actualizar_implemento.php?id_implemento=<?php echo $id_implemento; ?>" method="POST">
             <div class="form-group">
-                <label for="foto">Enlace a foto de insumo:</label>
+                <label for="foto">Enlace a foto de implemento:</label>
                 <div class="input-group">
                     <div class="input-group-prepend">
                         <span class="input-group-text"><i class="fas fa-link"></i></span>
                     </div>
-                    <input type="text" class="form-control" name="foto" value="<?php echo $insumo['foto']; ?>" required>
+                    <input type="text" class="form-control" name="foto" value="<?php echo $implemento['foto']; ?>" required>
                 </div>
             </div>
             <div class="form-group">
@@ -96,7 +97,7 @@ if (isset($_GET['id_insumo'])) {
                     <div class="input-group-prepend">
                         <span class="input-group-text"><i class="fas fa-boxes"></i></span>
                     </div>
-                    <input type="number" class="form-control" name="stock" value="<?php echo $insumo['stock_insumo']; ?>" required>
+                    <input type="number" class="form-control" name="stock" value="<?php echo $implemento['stock_implemento']; ?>" required>
                 </div>
             </div>
             <div class="form-group">
@@ -112,7 +113,7 @@ if (isset($_GET['id_insumo'])) {
                         $sql_ubicacion = "SELECT id_ubicacion FROM ubicacion ORDER BY id_ubicacion ASC";
                         $result_ubicacion = mysqli_query($conectar, $sql_ubicacion) or trigger_error("Error:", mysqli_error($conectar));
                         while ($row = mysqli_fetch_array($result_ubicacion)) {
-                            $selected = ($row['id_ubicacion'] == $insumo['ubicacion_fk']) ? 'selected' : '';
+                            $selected = ($row['id_ubicacion'] == $implemento['ubicacion_fk']) ? 'selected' : '';
                             echo "<option value='" . $row['id_ubicacion'] . "' $selected>" . $row['id_ubicacion'] . "</option>";
                         }
                         ?>
@@ -131,7 +132,7 @@ if (isset($_GET['id_insumo'])) {
                         $sql_medida = "SELECT * FROM unidad_medida ORDER BY id_medida ASC";
                         $result_medida = mysqli_query($conectar, $sql_medida) or trigger_error("Error:", mysqli_error($conectar));
                         while ($row = mysqli_fetch_array($result_medida)) {
-                            $selected = ($row['id_medida'] == $insumo['und_medida_fk']) ? 'selected' : '';
+                            $selected = ($row['id_medida'] == $implemento['und_medida_fk']) ? 'selected' : '';
                             echo "<option value='" . $row['id_medida'] . "' $selected>" . $row['nombre_medida'] . "</option>";
                         }
                         ?>
