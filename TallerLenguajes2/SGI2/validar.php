fa278de8e530b2a270640866e55511bcef5c1c6d
AnaMgS02 <anamariagomez102@gmail.com>
2024-10-01 23:42:50 -0500

    s

--- TallerLenguajes2/SGI2/validar.php
+++ TallerLenguajes2/SGI2/validar.php
@@ -15,7 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $stmt = mysqli_prepare($conectar, "SELECT * FROM acceso WHERE user = ? AND pass = ?");
     mysqli_stmt_bind_param($stmt, "ss", $usuario, $pass);
     mysqli_stmt_execute($stmt);
-    $resultado = mysqli_stmt_get_result($stmt) or trigger_error("Error: ", mysqli_error($conectar));
+    $resultado = mysqli_stmt_get_result($stmt) or mysqli_error($conectar);
     $total = mysqli_num_rows($resultado);
     if ($total > 0) {
         echo "<br>Usuario y clave correctos";
