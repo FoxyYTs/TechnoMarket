dbbec735543d887a21feb3b69a59d3219f676ca6
AnaMgS02 <anamariagomez102@gmail.com>
2024-10-01 20:58:58 -0500

    s

--- TallerLenguajes2/SGI2/app/insumo.php
+++ TallerLenguajes2/SGI2/app/insumo.php
@@ -127,14 +127,6 @@
                                 <label for="id_insumo"><i class="fas fa-barcode"></i> Código de Insumo:</label>
                                 <input type="text" class="form-control" placeholder="Ingrese el código de insumo" name="id_insumo" required>
                             </div>
-                            <div class="form-group">
-                                <label for="tipo_insumo"><i class="fas fa-list"></i> Tipo de Insumo:</label>
-                                <select class="form-control" name="tipo_insumo" required>
-                                    <option value="" selected disabled>Seleccione el tipo de insumo</option>
-                                    <option value="0">SUSTANCIA</option>
-                                    <option value="1">IMPLEMENTO</option>
-                                </select>
-                            </div>
                             <button type="submit" class="btn btn-success btn-block"><i class="fas fa-search"></i>
                                 Buscar</button>
                         </form>
@@ -146,67 +138,31 @@
 
     <!-- Mostrar insumo específico-->
     <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST" or isset($_GET['id_insumo'])) {
+    if ($_SERVER["REQUEST_METHOD"] == "POST" or isset($_GET['id_implemento'])) {
         include_once("db.php");
-        $id_insumo = isset($_GET['id_insumo']) ? $_GET['id_insumo'] : $_POST['id_insumo'];
-        $tipo_insumo = isset($_GET['tipo_insumo']) ? $_GET['tipo_insumo'] : $_POST['tipo_insumo'];
-        //$insumo = $_POST['id_insumo'];
-        //$tipo = $_POST['tipo_insumo'];
+        $id_implemento = isset($_GET['id_implemento']) ? $_GET['id_implemento'] : $_POST['id_implemento'];
 
         $conectar = conn(); // Conexión a la base de datos
-
-        if ($tipo_insumo == "0" or $tipo_insumo == "SUSTANCIA" or $tipo_insumo == "sustancia") {
-            $sql = "SELECT insumo.*, UPPER(insumo.tipo_insumo) AS tipo_insumo, 
-                    sustancia.id_sustancia, sustancia.ficha_seguridad, 
-                    sustancia.tel_emergencia, sustancia.formula_quimica, 
-                    sustancia.numCAS, palabra_seguridad.palabra, unidad_medida.nombre_medida 
-                    FROM insumo
-                    JOIN unidad_medida ON unidad_medida.id_medida = insumo.und_medida_fk
-                    JOIN sustancia ON sustancia.insumo_fk = insumo.id_insumo
-                    JOIN palabra_seguridad ON palabra_seguridad.id_palabra = sustancia.palabra_seguridad_fk
-                    WHERE insumo.id_insumo = '$id_insumo'";
-        } elseif ($tipo_insumo == "1" or $tipo_insumo == "IMPLEMENTO" or $tipo_insumo == "implemento") {
-            $sql = "SELECT insumo.*, UPPER(insumo.tipo_insumo) AS tipo_insumo, 
-                    implemento.id_implemento, implemento.guia_uso_lab, 
-                    unidad_medida.nombre_medida 
-                    FROM insumo
-                    JOIN unidad_medida ON unidad_medida.id_medida = insumo.und_medida_fk
-                    JOIN implemento ON implemento.insumoI_fk = insumo.id_insumo
-                    WHERE insumo.id_insumo = '$id_insumo'";
-        } else {
-            echo "<div class='container mt-4'><div class='alert alert-danger' role='alert'>El tipo proporcionado no existe.</div></div>";
-            exit;
-        }
+        $sql = "SELECT * FROM implemento JOIN unidad_medida ON unidad_medida.id_medida = implemento.und_medida_fk WHERE id_implemento = '$id_implemento'";
 
         $result = mysqli_query($conectar, $sql) or trigger_error("Error: ", mysqli_error($conectar));
 
         if ($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
                 echo '<div class="container mt-4"><div class="card text-center">';
-                echo '<div class="card-header"><h4 class="card-title">' . $row['nombre_insumo'] . '</h4></div>';
+                echo '<div class="card-header"><h4 class="card-title">' . $row['nombre_implemento'] . '</h4></div>';
                 echo '<div class="row g-0">';
-                echo '<div class="col-md-4"><img src="' . $row['foto'] . '" class="img-fluid rounded-start" alt="Foto ' . $row['nombre_insumo'] . '"></div>';
+                echo '<div class="col-md-4"><img src="' . $row['foto'] . '" class="img-fluid rounded-start" alt="Foto ' . $row['nombre_implemento'] . '"></div>';
                 echo '<div class="col-md-8"><div class="card-body"><ul class="list-group list-group-flush">';
-                echo '<li class="list-group-item"><h6>Insumo: </h6>' . $row['id_insumo'] . '</li>';
-                echo '<li class="list-group-item"><h6>Tipo: </h6>' . $row['tipo_insumo'] . '</li>';
-                echo '<li class="list-group-item"><h6>Stock: </h6>' . $row['stock_insumo'] . '</li>';
+                echo '<li class="list-group-item"><h6>Implemento: </h6>' . $row['id_implemento'] . '</li>';
+                echo '<li class="list-group-item"><h6>Stock: </h6>' . $row['stock_implemento'] . '</li>';
                 echo '<li class="list-group-item"><h6>Ubicación: </h6>' . $row['ubicacion_fk'] . '</li>';
                 echo '<li class="list-group-item"><h6>Unidad de medida: </h6>' . $row['nombre_medida'] . '</li>';
-                if ($tipo_insumo == "0" or $tipo_insumo == "SUSTANCIA" or $tipo_insumo == "sustancia") {
-                    echo '<li class="list-group-item"><h6>Sustancia activa: </h6>' . $row['id_sustancia'] . '</li>';
-                    echo '<li class="list-group-item"><h6>Emergencia: </h6>' . $row['tel_emergencia'] . '</li>';
-                    echo '<li class="list-group-item"><h6>Formula: </h6>' . $row['formula_quimica'] . '</li>';
-                    echo '<li class="list-group-item"><h6>CAS: </h6>' . $row['numCAS'] . '</li>';
-                    echo '<li class="list-group-item"><h6>Palabra de seguridad: </h6>' . $row['palabra'] . '</li>';
-                    echo '<li class="list-group-item"><a href="' . $row['ficha_seguridad'] . '" class="card-link">Ficha de seguridad</a></li>';
-                } else {
-                    echo '<li class="list-group-item"><h6>Implemento: </h6>' . $row['id_implemento'] . '</li>';
-                    echo '<li class="list-group-item"><h6>Guia de uso: </h6><p>' . $row['guia_uso_lab'] . '</p></li>';
-                }
-                echo '</ul></div></div></div></div></div>';
+                echo '<li class="list-group-item"><h6>Ficha tecnica: </h6>' . $row['ficha_tecnica'] . '</li>';
+                echo '<li class="list-group-item"><h6>Guia de uso: </h6><p>' . $row['guia_uso_lab'] . '</p></li>';
             }
         } else {
-            echo "<div class='container mt-4'><div class='alert alert-warning' role='alert'>No se encontraron resultados para el insumo proporcionado.</div></div>";
+            echo "<div class='container mt-4'><div class='alert alert-warning' role='alert'>No se encontraron resultados para el implemento proporcionado.</div></div>";
         }
     }
     ?>
