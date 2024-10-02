23c0ec94f47ee8c75f930de2724f9ae1025562b0
AnaMgS02 <anamariagomez102@gmail.com>
2024-10-02 13:59:46 -0500

    s

---
+++ TallerLenguajes2/SGI2/gestion_Tmaestras.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+
+// Verifica si hay una sesión iniciada
+if (!isset($_SESSION['user'])) {
+    // Redirige a la página de inicio de sesión si no hay sesión
+    header("Location: validar.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IMPLEMENTOS - SGI LAB MANAGER</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <style>
+        th,
+        td {
+            vertical-align: middle !important;
+        }
+    </style>
+</head>
+
+<body>
+    <!-- Menú de navegación -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand active" href="principal.php">LAB MANAGER</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="nav nav-tabs">
+                <?php
+                require 'funciones.php';
+                menu($_SESSION['user']);
+                ?>
+            </ul>
+            <span class="navbar-text">
+                <?php echo $_SESSION['user']; ?>
+            </span>
+        </div>
+    </nav>
+
+    <div class="d-grid gap-2 col-6 mx-auto">
+        <button class="btn btn-primary" type="button"><a href="reg_area.php">Nueva área</a></button>
+        <button class="btn btn-primary" type="button"><a href="reg_permisos.php">Nuevo permiso</a></button>
+        <button class="btn btn-primary" type="button"><a href="reg_roles.php">Nuevo rol</a></button>
+        <button class="btn btn-primary" type="button"><a href="reg_ubicacion.php">Nueva ubicación</a></button>
+        <button class="btn btn-primary" type="button"><a href="reg_und_medida.php">Nueva Unidad de Medida</a></button>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
